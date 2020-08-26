0dc0d66b4af6b2135f1879a558f10ffea9b46181
Jerry Kim <jerryrox@live.co.kr>
2020-08-26 14:42:36 +1200

    Fixed crash

--- lib/src/chat_view.dart
+++ lib/src/chat_view.dart
@@ -454,7 +454,8 @@ class DashChatState extends State<DashChat> {
           });
         }
       } else if (topReached) {
-        widget.onLoadEarlier();
+        if(widget.onLoadEarlier != null)
+          widget.onLoadEarlier();
       }
     });
   }
